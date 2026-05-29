217032cb4acf0df0d2c54e25db86b3f96460987b
smritikc <ismritikc1@gmail.com>
2026-05-29 19:51:36 +0545

    db setup

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,19 @@
-import express, { response } from "express";
-import { readFile } from "fs";
+import dotenv from "dotenv";
+dotenv.config();
 
-import fs from "fs/promises";
-import config from "./src/config/config.js";
-
-import productRoute from "./src/routes/product.route.js";
+import app from "./app.js";
 import connectDB from "./src/config/db.js";
 
-const app = express();
-app.use(express.json());
-
+/* =====================
+   Connect Database
+===================== */
 connectDB();
 
-app.get("/", (request, response) => {
-  response.send("Home Page");
-});
+/* =====================
+   Start Server
+===================== */
+const PORT = process.env.PORT || 5000;
 
-app.get("/about", (request, response) => {
-  response.send("About Page");
-});
-
-app.post("/contact", (request, response) => {
-  response.send("Contact form submitted");
-});
-
-app.get("/contact", (request, response) => {
-  response.send("Contact Page");
-});
-
-app.use("/api/products", productRoute);
-
-app.listen(config.port, () => {
-  console.log("Server running....");
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });

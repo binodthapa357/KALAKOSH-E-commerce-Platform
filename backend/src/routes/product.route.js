76c372004a34ccb7eebd273f94ec4799ae0fc1f3
binodthapa357 <bikashthapa9815648792@gmail.com>
2026-05-31 10:51:38 +0545

    updated

--- backend/src/routes/product.route.js
+++ backend/src/routes/product.route.js
@@ -7,9 +7,7 @@ const router = express.Router();
 router.get("/", productController.getAllProducts);
 router.get("/first", productController.getFirstProduct);
 router.get("/search", validateSearch, productController.searchProducts);
-//Dynamic Route (:param)
 router.get("/:id", productController.getProductByID);
-
 router.post("/", productController.createProduct);
 
 export default router;
